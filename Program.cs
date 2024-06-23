f7d5933428a9c9dda0e14b1aa75a11e959212a7b
ChielokaCode <realmatec01@gmail.com>
2024-06-23 09:10:40 +0100

    initial commit

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ global using hng_task1.Model;
 global using hng_task1.Controllers;
 global using hng_task1.Service;
 global using hng_task1.ServiceImpl;
+using Microsoft.AspNetCore.HttpOverrides; // Add this line for ForwardedHeaders
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,14 +11,20 @@ builder.Services.AddControllers();
 builder.Services.AddSingleton<ILogger<UserController>, Logger<UserController>>();
 builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddTransient<IIpService, IpService>();
-
+// Configure the forwarded headers middleware to read the correct client IP address
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
 var app = builder.Build();
+
+// Enable forwarded headers middleware in the request pipeline
+app.UseForwardedHeaders();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -25,6 +32,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
